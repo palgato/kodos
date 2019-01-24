f58d225f4a62942ea113a227cc993faab1dc13f5
palgato <davo.kelly@gmail.com>
2019-01-24 07:17:31 +0000

    Remote data storage read/write - Updated FileHandler readFromFile() and writeToFile() methods to use the AWS bucket storage instead of local storage

--- src/main/java/palgato/kodos/FileHandler.java
+++ src/main/java/palgato/kodos/FileHandler.java
@@ -1,20 +1,51 @@
 package palgato.kodos;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.*;
 
 public class FileHandler {
 
+    public static AmazonS3 accessBucket() {
+
+        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.EU_WEST_2)
+                .build();
+        return s3client;
+
+    }
+
     /* Read from File - reads a LeaderBoard file and returns a HashMap of Players and their wins */
     public static Map<String, Player> readFromFile(String filePath) {
+
+        String s3Bucket = System.getenv("S3_BUCKET_NAME");
+
+        AmazonS3 s3client = accessBucket();
+        S3Object s3object = s3client.getObject(s3Bucket, filePath);
+        S3ObjectInputStream inputStream = s3object.getObjectContent();
+
         String line;
         Map<String, Player> loadedPlayers = new HashMap<>();
-        File file = new File(filePath);
+        //File file = new File(filePath);
 
         try {
-            file.createNewFile();
-            FileReader fr = new FileReader(filePath);
-            BufferedReader br = new BufferedReader(fr);
+            //file.createNewFile();
+            //FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Charset.defaultCharset()));
 
             while ((line = br.readLine()) != null) {
                 List<String> aList = new ArrayList<>(Arrays.asList(line.split(",")));
@@ -33,12 +64,15 @@ public class FileHandler {
     /* Write to File - adds a Player and their number of wins to a LeaderBoard */
     public static void writeToFile(LeaderBoard leaderBoard) {
 
+        String s3Bucket = System.getenv("S3_BUCKET_NAME");
+
         String writeName;
         int writeWins;
         boolean writeActive;
 
         try {
-            FileWriter fw = new FileWriter(leaderBoard.getFilePath());
+            File file = new File(leaderBoard.getFilePath());
+            FileWriter fw = new FileWriter(file);
 
             //For each item in the boardPlayers HashMap, write the data to the file as comma separated values
             for (String key : leaderBoard.getBoardPlayers().keySet()) {
@@ -47,12 +81,15 @@ public class FileHandler {
                 writeActive = leaderBoard.getBoardPlayers().get(key).getActive();
 
                 fw.write(writeName + ",");
-                fw.write(Integer.toString(writeWins) + ",");
+                fw.write(writeWins + ",");
                 fw.write(Boolean.toString(writeActive));
                 fw.write("\n");
             }
             fw.flush();
             fw.close();
+
+            AmazonS3 s3client = accessBucket();
+            s3client.putObject(s3Bucket,leaderBoard.getFilePath(),file);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/palgato/kodos/KodosApplication.java
+++ src/main/java/palgato/kodos/KodosApplication.java
@@ -1,12 +1,5 @@
 package palgato.kodos;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -22,32 +16,17 @@ public class KodosApplication {
 
 	public static void main(String[] args) {
 
-		String accessKey = "CHANGE_ME";
-		String secretKey = "CHANGE_ME";
-		String s3Bucket = "kodosboard";
-
-		AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-		AmazonS3 s3client = AmazonS3ClientBuilder
-				.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-				.withRegion(Regions.EU_WEST_2)
-				.build();
-		S3Object s3object = s3client.getObject(s3Bucket, "unoBoard.csv");
-		S3ObjectInputStream inputStream = s3object.getObjectContent();
-		String readFile = readFile(inputStream);
-		System.out.println("readFile = " + readFile);
-
 		SpringApplication.run(KodosApplication.class, args);
 
 	}
 
-	private static String readFile(S3ObjectInputStream inputStream) {
+/*	private static String readFile(S3ObjectInputStream inputStream) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Charset.defaultCharset()))) {
 			return br.lines().collect(Collectors.joining(System.lineSeparator()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
-	}
+	}*/
 
 }
